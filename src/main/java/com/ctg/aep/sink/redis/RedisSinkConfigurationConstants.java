f45f6be26ff6c611b4216c8efe4cfd2acfeed73e
zhangwusheng <zhangwusheng@sina.com>
2018-01-29 21:53:34 +0800

    add redis config item

--- src/main/java/com/ctg/aep/sink/redis/RedisSinkConfigurationConstants.java
+++ src/main/java/com/ctg/aep/sink/redis/RedisSinkConfigurationConstants.java
@@ -18,64 +18,9 @@
  */
 package com.ctg.aep.sink.redis;
 
-import org.apache.hadoop.hbase.HConstants;
-
-/**
- * Constants used for configuration of HBaseSink and AsyncHBaseSink
- *
- */
 public class RedisSinkConfigurationConstants {
-  /**
-   * The Hbase table which the sink should write to.
-   */
-  public static final String CONFIG_TABLE = "table";
-  /**
-   * The column family which the sink should use.
-   */
-  public static final String CONFIG_COLUMN_FAMILY = "columnFamily";
-  /**
-   * Maximum number of events the sink should take from the channel per
-   * transaction, if available.
-   */
-  public static final String CONFIG_BATCHSIZE = "batchSize";
-  /**
-   * The fully qualified class name of the serializer the sink should use.
-   */
-  public static final String CONFIG_SERIALIZER = "serializer";
-  /**
-   * Configuration to pass to the serializer.
-   */
-  public static final String CONFIG_SERIALIZER_PREFIX = CONFIG_SERIALIZER + ".";
 
-  public static final String CONFIG_TIMEOUT = "timeout";
-
-  public static final String CONFIG_ENABLE_WAL = "enableWal";
-
-  public static final boolean DEFAULT_ENABLE_WAL = true;
-
-  public static final long DEFAULT_TIMEOUT = 60000;
-
-  public static final String CONFIG_KEYTAB = "kerberosKeytab";
-
-  public static final String CONFIG_PRINCIPAL = "kerberosPrincipal";
-
-  public static final String ZK_QUORUM = "zookeeperQuorum";
-
-  public static final String ZK_ZNODE_PARENT = "znodeParent";
-
-  public static final String DEFAULT_ZK_ZNODE_PARENT =
-      HConstants.DEFAULT_ZOOKEEPER_ZNODE_PARENT;
-
-  public static final String CONFIG_COALESCE_INCREMENTS = "coalesceIncrements";
-
-  public static final Boolean DEFAULT_COALESCE_INCREMENTS = false;
-
-  public static final int DEFAULT_MAX_CONSECUTIVE_FAILS = 10;
-
-  public static final String CONFIG_MAX_CONSECUTIVE_FAILS = "maxConsecutiveFails";
-  
-  public static final String AUTO_CREATE_NAMESPACE="autoCreateNamespace";
-  public static final String UBER_NAMESPACE_NAME="uberNamespaceName";
-  public static final String UBER_TABLE_NAME="uberTableName";
+  public static final String IP_ADDRESS = "host";
+  public static final String PORT = "port";
 
 }
