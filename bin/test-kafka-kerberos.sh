a406e521c6b7fce89a1374ed98fdd6d1bcf63855
zhangwusheng@sina.com <kent001>
2018-02-26 19:24:23 +0800

    add kafka jaas test

---
+++ bin/test-kafka-kerberos.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  BINDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="BINDIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+BINDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+DIR=`dirname ${BINDIR}`
+CLASSPATH="${DIR}/libs/*:/etc/hbase/conf:/etc/hadoop/conf:/etc/hive/conf"
+
+echo "DIR=${DIR}"
+export JAVA_HOME="/opt/jdk1.8.0_20"
+export JAVA_HOME="/opt/jdk1.7.0_79"
+
+rm -f ${DIR}/libs/slf4j-log4j12-*.jar
+java -Daep.home.dir=${DIR} -cp  "${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar:${CLASSPATH}" com.ctg.aep.dataserver.KafkaTestApplicaiton
+
