8f8f7ef822a7e8ad27406c0852c3d474694fdb24
zhangwusheng@sina.com <kent001>
2018-02-26 20:17:36 +0800

    add kafka jaas test

---
+++ src/main/java/com/ctg/aep/dataserver/ThreeTest.java
@@ -0,0 +1,8 @@
+package com.ctg.aep.dataserver;
+
+public class ThreeTest {
+
+   private String keyTab = "/etc/security/keytabs/odp.user.keytab";
+    private String principal = "odp/danalysis@DFS.COM";
+
+}
