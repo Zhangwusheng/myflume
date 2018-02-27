f1a9ce9af0fa66bf401df5129c268d7e73a161a4
zhangwusheng@sina.com <kent001>
2018-02-27 17:38:21 +0800

    refact to init and write

--- src/main/java/com/ctg/aep/dataserver/CtgCacheTest.java
+++ src/main/java/com/ctg/aep/dataserver/CtgCacheTest.java
@@ -4,32 +4,38 @@ import com.ctg.itrdc.cache.common.exception.CacheConfigException;
 import com.ctg.itrdc.cache.core.CacheService;
 import org.apache.flume.FlumeException;
 
+import java.util.*;
+
 public class CtgCacheTest {
 
+    CacheService cacheService;
+    String  groupId = "group.AEP.storage";
+    long timeout = 5000;
+    String user = "AEP";
+    String passwd = "Redis123";
+    String[] groups = {groupId};
 
-    public static void main(String[] args) {
+    public void init(){
 
-        CacheService cacheService;
-        String  groupId = "group.AEP.storage";
-        long timeout = 5000;
-        String user = "AEP";
-        String passwd = "Redis123";
-        String[] groups = {groupId};
         try {
             cacheService = new CacheService(groups,timeout,user,passwd);
-
-            for(int i=0;i<10000;i++) {
-                String code = cacheService.set(groupId, "itemKey"+i, "value"+i);
-
-                System.out.println("code=" + code);
-                String value = cacheService.get(groupId, "itemKey"+i);
-                System.out.println("value=" + value);
-            }
-
 //            cacheService.close();
         } catch (CacheConfigException e) {
             throw new FlumeException(e);
         }
+    }
+
+    public void testWriteAndRead(){
+        for(int i=0;i<10000;i++) {
+            String code = cacheService.set(groupId, "itemKey"+i, "value"+i);
+
+            System.out.println("code=" + code);
+            String value = cacheService.get(groupId, "itemKey"+i);
+            System.out.println("value=" + value);
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
